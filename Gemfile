462d5bcb03b68df402e815a29afb45af6fe6b2ef
Aaron Qian <aq1018@gmail.com>
2011-12-19 21:28:58 -0800

    upgrade mongoid and paperclip version, update code to remove deprication warnings

--- Gemfile
+++ Gemfile
@@ -1,17 +1,18 @@
 source 'http://rubygems.org'
 
-gem 'mongoid', '~> 2.0.0.beta.20'
-gem "mongoid-paperclip", '~> 0.0.3', :require => "mongoid_paperclip"
+gem 'mongoid', '~> 2.3.4'
+gem "mongoid-paperclip", '~> 0.0.7', :require => "mongoid_paperclip"
+gem "paperclip", '~> 2.4'
 
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
 group :development do
   gem 'database_cleaner'
-  gem 'bson', '~> 1.2.1'
-  gem 'bson_ext', '~> 1.2.1'
-  gem 'rspec', '~> 2.3.0'
-  gem 'yard', '~> 0.6.0'
-  gem 'bundler', '~> 1.0.0'
-  gem 'jeweler', '~> 1.5.2'
-  gem 'rcov', '>= 0'
+  gem 'bson'
+  gem 'bson_ext'
+  gem 'rspec'
+  gem 'yard'
+  gem 'bundler'
+  gem 'jeweler'
+  gem 'rcov'
 end
--- lib/mongoid_paperclip_image_dimension.rb
+++ lib/mongoid_paperclip_image_dimension.rb
@@ -2,12 +2,12 @@ require 'active_support/concern'
 
 module Mongoid::Paperclip::ImageDimension
   extend ActiveSupport::Concern
-  
+
   included do
-    class_inheritable_reader :perperclip_image_dimension_fields
-    write_inheritable_attribute(:perperclip_image_dimension_fields, [])
+    class_attribute :perperclip_image_dimension_fields
+    self.perperclip_image_dimension_fields = []
   end
-  
+
   module ClassMethods
     def save_image_dimensions_on(*args)
       args.flatten.each do |attachment_field|
@@ -15,25 +15,25 @@ module Mongoid::Paperclip::ImageDimension
         dimension_field_name = "#{attachment_field}_dimensions".to_sym
         perperclip_image_dimension_fields << attachment_field
         field dimension_field_name, :type => Hash
-        
+
         class_eval <<-END
           def #{attachment_field}_dimension(style=:original)
             #{attachment_field}_dimensions[style.to_s].map(&:to_i)
           end
-          
+
           def #{attachment_field}_dimension_str(style=:original)
             #{attachment_field}_dimension(style).join('x')
           end
-          
+
           after_#{attachment_field}_post_process do
             save_dimensions_for(:"#{attachment_field}")
           end
         END
-         
+
       end
     end
   end
-  
+
   def save_dimensions_for(attachment_field)
     styles = self.class.attachment_definitions[attachment_field][:styles].keys + [:original]
     dimension_hash = {}
